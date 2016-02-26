ed04a30c964f0fa2454a86f13050efb1de858fb2
KimothyPepper <kim-pep@hotmail.com>
2016-02-26 09:06:16 -0500

    Made Looper Triangle Files

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var newLetter = prompt('Enter a letter.');
+
+var i;
+
+for (i = 0; i < 1; i++) {
+  document.write(newLetter);
+  document.write('<br>');
+}
+
+for (i = 0; i < 2; i++) {
+  document.write(newLetter);
+}
+
+  document.write('<br>');
+
+for (i = 0; i < 3; i++) {
+  document.write(newLetter);
+}
+
+ document.write('<br>');
+
+for (i = 0; i < 4; i++) {
+  document.write(newLetter);
+}
+
+document.write('<br>');
+
+for (i = 0; i < 5; i++) {
+  document.write(newLetter);
+}
